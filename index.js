800c9db86f06dd2ac2d48e9b96b707f5b20b4789
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-06-18 18:46:03 -0600

    buildTree function

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ class Node {
 }
 
 class Tree {
-    constructor() {
-        this.root = this.buildTree();
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+    sortAndRemoveDuplicates(array) {
+        const result = [...new Set(array)].sort((a, b) => a - b);
+        return result;
+    }
+
+    buildTree(array) {
+        let sortedArray = this.sortAndRemoveDuplicates(array);
+        if (sortedArray.length === 0) return null;
+        const mid = parseInt(sortedArray.length / 2);
+        const root = new Node(
+            sortedArray[mid],
+            this.buildTree(sortedArray.slice(0, mid)),
+            this.buildTree(sortedArray.slice(mid + 1))
+        );
+        return root;
     }
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.key}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+}; 
+
+const tree = new Tree([1,2,3,4,5]);
+prettyPrint(tree.root);
